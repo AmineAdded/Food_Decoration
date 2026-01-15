2a64279b157efb1ae609d069f2d0e1d76f26704c
Amine Added <amineadded3@gmail.com>
2026-01-15 14:53:17 +0100

    fix SSR in angular

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideRouter(routes, withHashLocation()),  // ✅ Ajouter withHashLocation()
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
